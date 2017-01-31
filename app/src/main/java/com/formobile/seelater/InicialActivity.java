85050cf6b9dacf13658bf9d4e5a827eed452e53b
Caio Ferreira <caiobarbosaferreira@gmail.com>
2017-01-31 17:22:16 -0200

    Criação da tela inicial/Funcionalidade do botao começar

--- app/src/main/java/com/formobile/seelater/InicialActivity.java
+++ app/src/main/java/com/formobile/seelater/InicialActivity.java
@@ -1,12 +1,14 @@
 package com.formobile.seelater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class InicialActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String CATEGORIA = "Script";
     Button b1;
 
@@ -14,52 +16,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inicial);
 
-        Log.i(CATEGORIA, getClassName()+".onCreate();");
+        Log.i(CATEGORIA, getClassName() + ".onCreate();");
 
-        b1 = (Button) findViewById(R.id.buttonVoltar);
+        b1 = (Button) findViewById(R.id.buttonComecar);
         b1.setOnClickListener(this);
+
     }
 
     public void onStart() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onStart();");
+        Log.i(CATEGORIA, getClassName() + ".onStart();");
     }
 
     public void onRestart() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onRestart();");
+        Log.i(CATEGORIA, getClassName() + ".onRestart();");
     }
 
     public void onResume() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onResume();");
+        Log.i(CATEGORIA, getClassName() + ".onResume();");
     }
 
     public void onPause() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onPause();");
+        Log.i(CATEGORIA, getClassName() + ".onPause();");
     }
 
     public void onStop() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onStop();");
+        Log.i(CATEGORIA, getClassName() + ".onStop();");
     }
 
     public void onDestroy() {
         super.onStart();
-        Log.i(CATEGORIA, getClassName()+".onDestroy();");
+        Log.i(CATEGORIA, getClassName() + ".onDestroy();");
     }
     //CICLO DE VIDA
 
     private String getClassName() {
         String aux = getClass().getName();
-        return(aux.substring(aux.lastIndexOf(".") +1));
+        return (aux.substring(aux.lastIndexOf(".") + 1));
     }
 
     @Override
     public void onClick(View v) {
-        finish();
+        Intent intent = new Intent(InicialActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
