d881a22e3719d1eaaf1153f5b7616d0273eb7651
nathaliarezende <nathalia.silva.11@hotmail.com>
2017-02-17 22:14:00 -0200

    Desenvolvimento da tela Adicionar Serie

---
+++ app/src/main/java/com/formobile/seelater/AdicionarSerieActivity.java
@@ -0,0 +1,26 @@
+package com.formobile.seelater;
+
+/* Created by Nathalia on 16/02/2017. */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class AdicionarSerieActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionarserie);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() { super.onStop(); }
+}
