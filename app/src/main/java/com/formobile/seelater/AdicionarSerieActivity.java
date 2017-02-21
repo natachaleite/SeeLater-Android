321f53072c5d2eaf201f86b8f1c017e0fc49e22c
nathaliarezende <nathalia.silva.11@hotmail.com>
2017-02-20 21:09:46 -0300

    Classe AdicionarSerie

--- app/src/main/java/com/formobile/seelater/AdicionarSerieActivity.java
+++ app/src/main/java/com/formobile/seelater/AdicionarSerieActivity.java
@@ -3,10 +3,16 @@ package com.formobile.seelater;
 /* Created by Nathalia on 16/02/2017. */
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 
 public class AdicionarSerieActivity extends Activity {
+
 
 
     @Override
@@ -14,12 +20,35 @@ public class AdicionarSerieActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionarserie);
+
+        EditText etNome = (EditText) findViewById(R.id.etNome);
+        EditText etTemp = (EditText) findViewById(R.id.etTemp);
+        EditText etProdutora = (EditText) findViewById(R.id.etProdutora);
+        EditText etGenero = (EditText) findViewById(R.id.etGenero);
+        EditText etComentario = (EditText) findViewById(R.id.etComentario);
+        RatingBar rbPrioridade = (RatingBar) findViewById(R.id.rbPrioridade);
+
+        String nomeSerie = etNome.toString();
+        String numTemp = etTemp.toString();
+        String produtora = etProdutora.toString();
+        String genero = etGenero.toString();
+        String comentario = etComentario.toString();
+        int prioridade = rbPrioridade.getNumStars();
+
+        Button buttonOK = (Button) findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
+
+
+
     @Override
-    public void onStart() {
-        super.onStart();
-    }
+    public void onStart() { super.onStart(); }
 
     @Override
     public void onStop() { super.onStop(); }
--- app/src/main/java/com/formobile/seelater/InicialActivity.java
+++ app/src/main/java/com/formobile/seelater/InicialActivity.java
@@ -1,75 +1,13 @@
 package com.formobile.seelater;
 
-<<<<<<< HEAD
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.util.Log;
-import android.widget.Button;
-
-
-public class InicialActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String CATEGORIA = "Script";
-    Button b1;
-
-    //CICLO DE VIDA
-=======
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class InicialActivity extends AppCompatActivity {
 
->>>>>>> dev
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
-<<<<<<< HEAD
-
-        Log.i(CATEGORIA, getClassName() + ".onCreate();");
-    }
-
-    public void onStart() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onStart();");
-    }
-
-    public void onRestart() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onRestart();");
-    }
-
-    public void onResume() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onResume();");
-    }
-
-    public void onPause() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onPause();");
-    }
-
-    public void onStop() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onStop();");
-    }
-
-    public void onDestroy() {
-        super.onStart();
-        Log.i(CATEGORIA, getClassName() + ".onDestroy();");
-    }
-    //CICLO DE VIDA
-
-    private String getClassName() {
-        String aux = getClass().getName();
-        return (aux.substring(aux.lastIndexOf(".") + 1));
-    }
-
-    @Override
-    public void onClick(View v) {
-        finish();
-=======
->>>>>>> dev
     }
 }
